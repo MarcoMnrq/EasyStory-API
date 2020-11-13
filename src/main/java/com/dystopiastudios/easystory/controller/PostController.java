e859e0cbccafa8cab90043ded606e731d2f6f397
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2020-11-13 02:03:19 -0500

    fix post controller

--- src/main/java/com/dystopiastudios/easystory/controller/PostController.java
+++ src/main/java/com/dystopiastudios/easystory/controller/PostController.java
@@ -21,6 +21,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +29,7 @@ import java.util.stream.Collectors;
 @Tag(name = "posts", description = "Posts API")
 @RestController
 @RequestMapping("/api")
+@Transactional
 @CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 
